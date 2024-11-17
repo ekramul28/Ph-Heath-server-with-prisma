176035aacab7612d1c46a207037cca1df431e6c6
Ekramul <mdekramulhassan168@gmail.com>
2024-11-17 08:03:59 +0600

    some code added

--- src/app/modules/User/user.router.ts
+++ src/app/modules/User/user.router.ts
@@ -2,12 +2,56 @@ import express from "express";
 import { UserController } from "./user.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import { fileUploader } from "../../helpars/fileUploader";
 
 const router = express.Router();
+
+import { v2 as cloudinary } from "cloudinary";
+(async function () {
+  // Configuration
+  cloudinary.config({
+    cloud_name: "dvtdneocc",
+    api_key: "625384168839741",
+    api_secret: "TK5cNSBfnvosPvnT55g8lehKhD0", // Click 'View API Keys' above to copy your API secret
+  });
+
+  // Upload an image
+  const uploadResult = await cloudinary.uploader
+    .upload(
+      "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+      {
+        public_id: "shoes",
+      }
+    )
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(uploadResult);
+
+  // Optimize delivery by resizing and applying auto-format and auto-quality
+  const optimizeUrl = cloudinary.url("shoes", {
+    fetch_format: "auto",
+    quality: "auto",
+  });
+
+  console.log(optimizeUrl);
+
+  // Transform the image: auto-crop to square aspect_ratio
+  const autoCropUrl = cloudinary.url("shoes", {
+    crop: "auto",
+    gravity: "auto",
+    width: 500,
+    height: 500,
+  });
+
+  console.log(autoCropUrl);
+})();
 
 router.post(
   "/",
   auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  fileUploader.upload.single("file"),
   UserController.createAdmin
 );
 
